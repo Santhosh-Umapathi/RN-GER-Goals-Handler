c03a770e231f79ddc67511d249742545f729a16c
Santhosh Umapathi <Santhosh.professional1@gmail.com>
2020-04-14 18:03:56 +0200

    Initial Commit

---
+++ App.js
@@ -0,0 +1,24 @@
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import HomeScreen from './src/screens/HomeScreen';
+
+const MainNavigator = createStackNavigator(
+{
+  Home: HomeScreen,
+},
+{
+  initialRouteName: 'Home',
+  defaultNavigationOptions: {
+  title: 'Goals Handler',
+  headerTintColor: 'white', //Header button colors
+  headerStyle: {
+    backgroundColor: 'lightblue' // header bg color
+    },
+  headerTitleStyle: 
+  {
+    color: 'black' // header font color
+  }
+    }
+      }
+  );
+export default createAppContainer(MainNavigator);
