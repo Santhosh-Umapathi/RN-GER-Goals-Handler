216e4009d55edccf89bc834b9ae9ba4041bb4d5d
Santhosh Umapathi <Santhosh.professional1@gmail.com>
2020-04-14 18:32:16 +0200

    input handler complete

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -8,23 +8,58 @@ export default class HomeScreen extends Component
 		super(props);
 		this.state =
 		{
-			text:''
+			text: '',
+			goals: []
 		};
 	}
+
+
+	//TODO: method to handle user input
+	enteredText = (text) => {
+		this.setState({ text });
+		console.log(this.state.text)
+	};
+	
+	//TODO: method to save goals to array
+	saveGoal = () => {
+
+	};
 
 
 	render()
 	{
 		return (
-			<View>
-				<Text> HomeScreen </Text>
-			</View>
-			);
+      <View style={styles.containerView}>
+		<View style={styles.handlerStyle}>
+          <TextInput
+            placeholder="Enter Goals"
+            onChangeText={(text) => { this.enteredText(text) }}
+			value={this.state.text}
+            style={styles.textInputStyle}
+          />
+          <Button title="Add" onPress={() => {}} />
+				</View>
+			
+      </View>
+    );
 	}
 }
 
 const styles = StyleSheet.create({
-	containerView:
-	{
-		flex:1
-	}
+  containerView: {
+    flex: 1,
+  },
+  textInputStyle: {
+    borderColor: "lightblue",
+    borderWidth: 0.5,
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+	margin: 10,
+	width: "80%"
+  },
+	handlerStyle: {
+		flexDirection: 'row',
+		justifyContent: 'space-evenly',
+		alignItems:'center'
+  }
+});
